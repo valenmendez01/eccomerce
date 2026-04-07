95f4b17d62550e6509429fc668b649e45b8c34ae
matiabeledo <matiabeledo@gmail.com>
2026-04-07 19:56:10 -0300

    Las 2 cosas
    * Agregado todo lo de usuario
    
    * Arreglado id productos y funcionando

--- src/main/java/com/uade/eccomerce/repository/UsuarioRepository.java
+++ src/main/java/com/uade/eccomerce/repository/UsuarioRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.uade.eccomerce.entity.Usuario;
+import java.util.Optional;
+
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findById(Long Id);
     
+    Optional<Usuario> findByEmail(String email);
 }
