acb08511e10adf661d1a76f43a944feac96a71ef
valenmendez01 <mendezvalentin824@gmail.com>
2026-04-08 17:28:53 -0300

    fix de unas cosas v3

--- src/main/java/com/uade/eccomerce/controllers/usuarios/CambiarContraRequest.java
+++
@@ -1,9 +0,0 @@
-// package com.uade.eccomerce.controllers.usuarios;
-
-// import lombok.Data;
-
-// @Data
-// public class CambiarContraRequest {
-//     private String contraVieja;
-//     private String contraNueva;
-// }
